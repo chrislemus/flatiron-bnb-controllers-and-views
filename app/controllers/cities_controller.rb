49b86781918f2220f40bfeefcefa632a70618288
chrislemus <icrislemus@gmail.com>
2021-02-23 10:54:48 -0500

    Done.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,2 +1,11 @@
 class CitiesController < ApplicationController
+
+  def index
+    @cities = City.all
+  end
+
+  def show
+    @city = City.find(params[:id])
+    @listings = @city.listings
+  end
 end
